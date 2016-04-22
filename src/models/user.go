555d4f4855a00d1930f4fc435085dd360fd2bd06
YanshuoH <yanshuo.huang@gmail.com>
2016-04-22 18:46:31 +0800

    func for lib/statistic

--- src/models/user.go
+++ src/models/user.go
@@ -18,6 +18,31 @@ type User struct {
   Books     []interface{} `json:"collections" binding:"required" bson:"books"`
   CreatedOn time.Time `json:"created_on" bson:"created_on" binding:"required"`
 	UpdatedOn time.Time `json:"updated_on" bson:"updated_on" binding:"required"`
+  Stat      map[string]*StatEntity `json:"stat" bson:"stat"`
+}
+
+type StatEntity struct {
+  Year int `json:"year"`
+  Count int `json:"count"`
+  Price float64 `json:"price"`
+  Rating map[string]int `json:"rating"`
+  Month map[string]int `json:"month"`
+  Author map[string]int `json:"author"`
+  Translator map[string]int `json:"translator"`
+  Publisher map[string]int `json:"publisher"`
+  Tags map[string]int `json:"tags"`
+  Posters []map[string]string `json:"posters"`
+}
+
+type BookEntity struct {
+  Rating map[string]interface{} `json:"rating"`
+  Author []string `json:"author"`
+  Translator []string `json:"translator"`
+  Tags []map[string]interface{} `json:"tags"`
+  Images map[string]string `json:"images"`
+  Publiser string `json:"publisher"`
+  Price string `json:"price"`
+  Title string `json:"title"`
 }
 
 func CastApiToModel(userInfo map[string]interface{}, bookCollection []interface{}) *User {
