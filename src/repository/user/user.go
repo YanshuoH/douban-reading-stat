68f24a7f0ec0fad1eac4e892784512a1c3f22f57
YanshuoH <yanshuo.huang@gmail.com>
2016-04-22 18:49:36 +0800

    func for repository/user Get

--- src/repository/user/user.go
+++ src/repository/user/user.go
@@ -9,6 +9,19 @@ import(
   "models"
 )
 
+func Get(userId string, con *mgo.Database) (models.User, error) {
+  user := models.User{}
+
+  // Try to get user by id
+  err := con.C(models.CollectionUser).Find(bson.M{"userId": userId}).One(&user)
+  // Try to get user by real name
+  if err != nil {
+    err = con.C(models.CollectionUser).Find(bson.M{"name": userId}).One(&user)
+  }
+
+  return user, err
+}
+
 func Save(user models.User, con *mgo.Database) (models.User, error) {
   if time.Time.IsZero(user.CreatedOn) {
     user.CreatedOn = time.Now()
