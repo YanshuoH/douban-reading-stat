ee7ae4b3860b789ed1231a2b803a1ef6399742cd
YanshuoH <yanshuo.huang@gmail.com>
2016-04-25 00:29:38 +0800

    add 2 more cases for fetcher test

--- src/lib/fetcher/username_test.go
+++ src/lib/fetcher/username_test.go
@@ -11,6 +11,8 @@ const (
   URL_TEST_3 = "https://www.douban.com/people/test123/"
   URL_TEST_4 = "https://www.douban.com/people/"
   URL_TEST_5 = "https://www.douban.com/nein/74783952/"
+  URL_TEST_6 = "https://www.douban.com/people/test-123/"
+  URL_TEST_7 = "https://www.douban.com/people/test-123--/"
 )
 
 func TestFetchUsername(t *testing.T) {
@@ -35,4 +37,12 @@ func TestFetchUsername(t *testing.T) {
   _, err = FetchUsername(URL_TEST_5)
   assert.NotNil(err, "Should be an error")
   assert.EqualError(err, "not found", "Fetcher", "Should be a not found error")
+
+  username, err = FetchUsername(URL_TEST_6)
+  assert.Nil(err, "Fetcher", "Should not be an error for int username")
+  assert.Equal("test-123", username, "Fetcher", "Should be the user test-123")
+
+  username, err = FetchUsername(URL_TEST_7)
+  assert.Nil(err, "Fetcher", "Should not be an error for int username")
+  assert.Equal("test-123--", username, "Fetcher", "Should be the user test-123--")
 }
