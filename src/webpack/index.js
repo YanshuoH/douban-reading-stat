497f6ded2cb0f4e28cf628aaa5101e9d710d1206
YanshuoH <yanshuo.huang@gmail.com>
2016-04-24 23:09:51 +0800

    webpack source files

---
+++ src/webpack/index.js
@@ -0,0 +1,13 @@
+require('./style/bootstrap.custom.css')
+require('./style/custom.css')
+require('../../node_modules/font-awesome/css/font-awesome.min.css')
+
+require('!!script!../../node_modules/angular2/bundles/angular2-polyfills.min.js')
+require('!!script!../../node_modules/rxjs/bundles/Rx.umd.min.js')
+require('!!script!../../node_modules/angular2/bundles/angular2-all.umd.min.js')
+require('!!script!../../node_modules/jquery/dist/jquery.min.js')
+require('!!script!../../node_modules/bootstrap/dist/js/bootstrap.min.js')
+require('!!script!../../node_modules/highcharts/highcharts.js')
+
+require('./js/hicharts-theme.js')
+// require('./js/boot')
