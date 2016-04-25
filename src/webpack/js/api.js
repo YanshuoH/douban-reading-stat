9a8f0cc2b353eb0bf3c27c3d0538d30a1d800b7b
YanshuoH <yanshuo.huang@gmail.com>
2016-04-25 13:48:29 +0800

    add getStat api request

--- src/webpack/js/api.js
+++ src/webpack/js/api.js
@@ -2,8 +2,15 @@ import $ from 'jquery'
 
 class Api {
   static getUser(url, doneFunc, failFunc) {
-    let request = $.ajax({
+    $.ajax({
       url: '/api/user?url=' + encodeURIComponent(url),
+      contentType: 'application/json; charset=utf-8',
+    }).done(doneFunc).fail(failFunc)
+  }
+
+  static getStat(userId, doneFunc, failFunc) {
+    $.ajax({
+      url: '/api/user/' + userId,
       contentType: 'application/json; charset=utf-8',
     }).done(doneFunc).fail(failFunc)
   }
