fd13e6f2a38578f000080a3c8648c85f0e75abdb
YanshuoH <yanshuo.huang@gmail.com>
2016-04-25 13:49:05 +0800

    handle nav transition

--- src/webpack/js/boot.js
+++ src/webpack/js/boot.js
@@ -2,5 +2,13 @@ import $ from 'jquery'
 import form from './form'
 
 $(() => {
+  $(window).scroll(function () {
+      let top = $(document).scrollTop()
+      if(top > 50)
+        $('#home > .navbar').removeClass('navbar-transparent')
+      else
+        $('#home > .navbar').addClass('navbar-transparent')
+  });
+
   form()
 })
